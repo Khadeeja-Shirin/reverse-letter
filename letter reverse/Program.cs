85f76055a058eff112bb8756e61abb67a9571de4
Khadeeja-Shirin <113839381+Khadeeja-Shirin@users.noreply.github.com>
2022-09-18 18:40:29 +0530

    Add files via upload

---
+++ letter reverse/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace letter_reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word= "", rev = "";
+            int len = 0;
+            Console.WriteLine("Enter a word");
+            word = Console.ReadLine();
+            len = word.Length - 1;
+            while (len >= 0)
+            {
+                rev = rev + word[len];
+                len--;
+            }
+            Console.WriteLine("\nReverse of word is = " + rev);
+            Console.ReadLine();
+        }
+
+    }
+    }
